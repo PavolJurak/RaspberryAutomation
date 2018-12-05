f3bd74a1067c01fbae65c04e862a09e9e93931e1
Pavol Jurak <pavol.jurak1@gmail.com>
2018-12-06 00:27:49 +0100

    1

--- homeAutomation/app.py
+++ homeAutomation/app.py
@@ -1,7 +1,19 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for, request
 from rpi_rf import RFDevice
-from homeAutomation.help.graph_generator import GraphTemp
+from help.graph_generator import GraphTemp
+
+command_codes = {'B_Light1': {'ON': '5393', 'OFF': '5293'},
+                'B_Light2': {'ON': '5193', 'OFF': '5093'}
+                }
+lights = [light_name for light_name in command_codes.keys()]
+
+
+def send_rf_code(code):
+    rf_pin = 17
+    rfdevice = RFDevice(rf_pin)
+    rfdevice.enable_tx()
+    rfdevice.tx_code(code,1,350)
 
 app = Flask(__name__)
 
@@ -9,6 +21,15 @@ app = Flask(__name__)
 @app.route("/home")
 def index():
     return render_template('home.jinja')
+
+@app.route("/control_light", methods=['GET'])
+def control_light():
+    if request.args.get('B_Light1') is not None:
+        value = request.args.get('B_Light1')
+        if value == 'ON':
+            send_rf_code(5393)
+        elif value == 'OFF':
+            send_rf_code(5293)
 
 @app.route('/led')
 def led_lamp():
@@ -27,8 +48,3 @@ def set_lights():
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
-
-def send_rf_code(code):
-    rf_pin = 17
-    rfdevice = RFDevice(rf_pin)
-    rfdevice.enable_tx(code,1,350)
--- homeAutomation/help/rf_send.py
+++
@@ -1,8 +0,0 @@
-from rpi_rf import RFDevice
-
-rf_pin = 17
-rfdevice = RFDevice(rf_pin)
-
-
-rfdevice.enable_tx()
-
